ca54d1509b8d0319f6ac0a1edecb3cd85dde8a5c
Andreas Skielboe <askielboe@askielboe.local>
2010-05-17 17:55:32 +0200

    Fixed some bugs in the response convultion code.

--- response.f90
+++ response.f90
@@ -2,7 +2,6 @@ program response
 
 use xspec_params
 
-
 implicit none
 
 !REAL,DIMENSION(nbins)		::	lowpar1, lowpar2, lowpar3, lowpar4
@@ -16,54 +15,40 @@ INTEGER, PARAMETER			:: mresp=1024, nresp=1070
 REAL,DIMENSION(nbins)	::	subspectrum, rdummy
 real, DIMENSION(1024,1070) :: x,q
 
-
 REAL,DIMENSION(1070)	:: synthspec, prospec, dummyspec
 REAL,DIMENSION(1024) :: rspec
 REAL,DIMENSION(nresp,mresp) :: resp
 REAL							:: chisquare, ss
 
-
-
-
-
 !!------Read in response matrix
-open(1,file='final_response.txt',form='formatted')
-			do j=1,1024
-         read(1,*) (x(j,i),i=1,1070)
-		enddo
-		
-		
+open(1,file='response_matrix.txt',form='formatted')
+		do j=1,1024
+			read(1,*) (x(j,i),i=1,1070)
+		end do
+close(1)
+
 !! Read in dummy spectrum
- do i=1,N
  	open(1,file='test.spectrum.txt',form='formatted')
- 	do j = 1,nbins
- 		read(1,'(i4, f15.0)') idummy, dummyspec(i)
- 	end do
+ 		do i = 1,nbins
+ 			read(1,'(i4, f15.0)') idummy, dummyspec(i)
+ 		end do
  	close(1)
- end do
-			
-		
+
 !! Convolution of the spectrum with response file
 
-do i=0,1024
-rspec(i)=dot_product(x(i,:),dummyspec(:))
-enddo
+do i=1,1024
+! 	rspec(i) = 0.
+! 	do j=1,1070
+! 		rspec(i) = rspec(i) + x(i,j)*dummyspec(j)
+! 	end do
+	rspec(i)=dot_product(x(i,:),dummyspec(:))
+end do
 
 !Write out rspec
 open(1,file='rspec.txt',form='formatted')
-do i = 1,1024
-	write(1,'(i4, a1, e15.4)') i, ' ', rspec(i)
-end do
+	do i = 1,1024
+		write(1,'(i4, a1, e15.4)') i, ' ', rspec(i)
+	end do
 close(1)
-
-
-		 
-		 
-close(1)
-
-
-
-
-
 
 end program response

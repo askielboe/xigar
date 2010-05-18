a40792e72b82f3b86a671f9d5570fce716462d7b
Martina <smarties@Zambo.local>
2010-05-18 11:47:50 +0200

    Changed temperature range to .3-11.0 keV

---
+++ xspec_params.f90
@@ -0,0 +1,5 @@
+module xspec_params
+implicit none
+INTEGER,PARAMETER :: nchannels=1070, nspectra=100.
+REAL,PARAMETER :: param_min=0.1, param_max=15., exposure=4179600.2, param_break=3.
+end module xspec_params

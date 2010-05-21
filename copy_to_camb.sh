3ceb1e4aea5d1a0fb4361f118175325f98b86ab1
Andreas Skielboe <askielboe@askielboe.local>
2010-05-21 15:43:45 +0200

    Added params_xray.ini for use in cosmomc. This will be copied over along with source code by using copy_to_camb.

---
+++ copy_to_camb.sh
@@ -0,0 +1,5 @@
+cp xigar.f90 ../cosmomc/source/xigar.f90
+cp sphvol.f90 ../cosmomc/source/sphvol.f90
+cp fit_params.f90 ../cosmomc/source/fit_params.f90
+cp xigar_params.f90 ../cosmomc/source/xigar_params.f90
+cp params_xray.ini ../cosmomc/params_xray.ini
